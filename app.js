56977c9a8e8d83920fd7503477fbbaf1283fecf9
kotorieclair <kotorieclair@gmail.com>
2015-07-27 22:57:17 +0900

    init github pages

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 3000
-
-app.use(express.static('./dest'));
-
-app.listen(port, function() {
-  console.log("server starting at port: " + port);
-});
